ca4330f4af7262207a011f4369402ce1c37b1be7
msajidh <sajidh@outlook.com>
2021-05-31 01:45:38 +0300

    error
    $select_id = $mysqli->select_id;
    
    $update_id = $mysqli->update_id;

---
+++ ajax/components/updates/db_update_orderdetails.php
@@ -0,0 +1,101 @@
+<?php
+// Updates the quantity of a book in a customer's orderdetails, through the `orderdetails` table in database `bookstore`. Takes in input the quantity to be updated and outputs a JSON with response code and update id.
+
+// **REQUIRES**
+// A JSON object named "orderdetails_update_input_query" must be POSTed to this file with the following key-value pairs:
+ 
+// orderdetails_update_input_query = 
+//     {
+//         Key: "orderId"
+//         Value: the customer's order id - should be able to be converted into type int(11) 
+
+//         Key: "quantity"
+//         Value: the quantity of books - should be able to be converted into type int(4)
+
+//         Key: "bookId" 
+//         Value: the book's database id (i.e, ISBN) - should be able to be converted into type varchar(13)
+
+//     }
+
+// **ENSURES**
+// a row is updated in the orderdetails table in the database with the passed attributes.
+// A JSON response is outputted, "orderdetails_update_output_response" which is formed as:
+
+// orderdetails_update_output_response = 
+//     {  
+//         "response_code": <RESPONSE CODE>,
+//         "response": <UPDATE ID>
+//     }
+
+
+// DEBUGGING TOOL - COMMENT OUT LATER - TO DO! 🔲
+include "../../../debug/chromephp-master/ChromePhp.php";
+
+require "../../../internal/dbconnect.php";
+session_start();
+
+$sql = "UPDATE `orderdetails` SET `quantity`=? WHERE `orderId`=? and `bookId`=?";
+
+// log to console
+ChromePhp::log($sql);
+
+// decode the JSON POSTed variable
+$update_input_query_encoded = $_REQUEST["orderdetails_update_input_query"];
+$update_input_query = json_decode($update_input_query_encoded);
+
+
+// Parse the POST-ed JSON input object into individual attributes
+$orderId = number_format($update_input_query->orderId);
+ChromePhp::log("\norderId=$orderId");
+
+$quantity = number_format($update_input_query->quantity);
+ChromePhp::log("\nquantity=$quantity");
+
+$bookId = $update_input_query->bookId;
+ChromePhp::log("\nbookId=$bookId");
+
+
+
+// prepares the SQL statement
+$stmt = $mysqli->prepare($sql);
+
+ChromePhp::log("Query Prepared");
+
+
+// checks for errors
+if(! $stmt) 
+{	
+    echo "ERROR:: ".$mysqli->error;
+}
+
+// binds parameters to their respective datatypes in the database
+$stmt->bind_param("iis", $orderId, $quantity, $bookId);
+
+ChromePhp::log("Parameters Bound");
+
+// executes statement
+$stmt->execute();
+
+
+ChromePhp::log("SQL Executed");
+
+// get the id of the update
+$update_id = $mysqli->update_id;
+
+// REMOVE - TO DO 🔲
+ChromePhp::log("Updated orderdetails: orderdetails_id=$update_id");
+$response = json_decode("{}");
+$response->response_code = $stmt->error;
+if ($stmt->error == "")
+{
+    $response->response_code = "success";
+}
+
+$response->response = "$update_id";
+$orderdetails_update_output_response = json_encode($response);
+
+// REMOVE - TO DO 🔲
+ChromePhp::log("\nComponent Output Response=$orderdetails_update_output_response");
+
+print $orderdetails_update_output_response;
+?>
